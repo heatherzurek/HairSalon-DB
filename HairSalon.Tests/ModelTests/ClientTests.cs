9938eea274e95b05f29ac6f0783a9767bb3ca95e
Heather Zurek <heather.zurek@gmail.com>
2019-05-10 10:18:01 -0700

    connecting database

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -9,211 +9,70 @@ namespace HairSalon.Tests
   [TestClass]
   public class ClientTest
   {
-      // public ClientTest()
-      // {
-      //     DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=hair_salon_test;";
-      // }
+
+    // public void Dispose()
+    // {
+    //   Client.ClearAll();
+    // }
+
+    // public ClientTest()
+    // {
+    //   DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=hair_salon_test;";
+    // }
 
       [TestMethod]
       public void ClientConstructor_CreatesInstanceOfClient_Client()
       {
         //arrange
-        Client newClient = new Client();
+        Client newClient = new Client("name");
 
         //assert
         Assert.AreEqual(typeof(Client), newClient.GetType());
       }
       //
-      // [TestMethod]
-      // public void GetDescription_ReturnsDescription_String()
-      // {
-      //   //Arrange
-      //   string description = "Walk the dog.";
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Item newItem = new Item(description, itemDueDate, 1);
+      [TestMethod]
+      public void GetName_ReturnsName_String()
+      {
+        //Arrange
+        string name = "alex";
+        Client newClient = new Client(name);
+
+        //Act
+        string result = newClient.GetName();
+
+        //Assert
+        Assert.AreEqual(name, result);
+      }
       //
-      //   //Act
-      //   string result = newItem.GetDescription();
+      [TestMethod]
+      public void SetName_SetName_String()
+      {
+        //Arrange
+        string name = "alex";
+        Client newClient = new Client(name);
+
+        //Act
+        string updatedName = "bryan";
+        newClient.SetName(updatedName);
+        string result = newClient.GetName();
+
+        //Assert
+        Assert.AreEqual(updatedName, result);
+      }
       //
-      //   //Assert
-      //   Assert.AreEqual(description, result);
-      // }
-      //
-      // [TestMethod]
-      // public void SetDescription_SetDescription_String()
-      // {
-      //   //Arrange
-      //   string description = "Walk the dog.";
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Item newItem = new Item(description, itemDueDate, 1);
-      //
-      //   //Act
-      //   string updatedDescription = "Do the dishes";
-      //   newItem.SetDescription(updatedDescription);
-      //   string result = newItem.GetDescription();
-      //
-      //   //Assert
-      //   Assert.AreEqual(updatedDescription, result);
-      // }
-      //
-      // [TestMethod]
-      // public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
-      // {
-      //   //Arrange
-      //   List<Item> newList = new List<Item> { };
-      //
-      //   //Act
-      //   List<Item> result = Item.GetAll();
-      //
-      //   //Assert
-      //   CollectionAssert.AreEqual(newList, result);
-      // }
-      //
-      // [TestMethod]
-      // public void GetAll_ReturnsItems_ItemList()
-      // {
-      //   //Arrange
-      //   string description01 = "Walk the dog";
-      //   string description02 = "Wash the dishes";
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Item newItem1 = new Item(description01, itemDueDate, 1);
-      //   newItem1.Save();
-      //   Item newItem2 = new Item(description02, itemDueDate, 1);
-      //   newItem2.Save();
-      //   List<Item> newList = new List<Item> { newItem1, newItem2 };
-      //
-      //   //Act
-      //   List<Item> result = Item.GetAll();
-      //
-      //   //Assert
-      //   CollectionAssert.AreEqual(newList, result);
-      // }
-      //
-      // [TestMethod]
-      // public void Save_AssignsIdToObject_Id()
-      // {
-      //   //Arrange
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Item testItem = new Item("Mow the lawn", itemDueDate, 1);
-      //
-      //   //Act
-      //   testItem.Save();
-      //   Item savedItem = Item.GetAll()[0];
-      //
-      //   int result = savedItem.GetId();
-      //   int testId = testItem.GetId();
-      //
-      //   //Assert
-      //   Assert.AreEqual(testId, result);
-      // }
-      //
-      // // [TestMethod]
-      // // public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
-      // // {
-      // //   //Arrange
-      // //   string description = "Walk the dog.";
-      // //   Item newItem = new Item(description, 1);
-      // //
-      // //   //Act
-      // //   int result = newItem.GetId();
-      // //
-      // //   //Assert
-      // //   Assert.AreEqual(1, result);
-      // // }
-      //
-      // [TestMethod]
-      // public void Find_ReturnsCorrectItemFromDatabase_Item()
-      // {
-      //   //Arrange
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Item testItem = new Item("Mow the lawn", itemDueDate, 1);
-      //   testItem.Save();
-      //
-      //   //Act
-      //   Item foundItem = Item.Find(testItem.GetId());
-      //
-      //   //Assert
-      //   Assert.AreEqual(testItem, foundItem);
-      // }
-      //
-      // [TestMethod]
-      // public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
-      // {
-      //   // Arrange, Act
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Item firstItem = new Item("Mow the lawn", itemDueDate, 1);
-      //   Item secondItem = new Item("Mow the lawn", itemDueDate, 1);
-      //
-      //   // Assert
-      //   Assert.AreEqual(firstItem, secondItem);
-      // }
-      //
-      // [TestMethod]
-      // public void Save_SavesToDatabase_ItemList()
-      // {
-      //   //Arrange
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Item testItem = new Item("Mow the lawn", itemDueDate, 1);
-      //
-      //   //Act
-      //   testItem.Save();
-      //   List<Item> result = Item.GetAll();
-      //   List<Item> testList = new List<Item>{testItem};
-      //
-      //   //Assert
-      //   CollectionAssert.AreEqual(testList, result);
-      // }
-      //
-      // [TestMethod]
-      // public void Edit_UpdatesItemInDatabase_String()
-      // {
-      //   //Arrange
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   string firstDescription = "Walk the Dog";
-      //   Item testItem = new Item(firstDescription, itemDueDate, 1);
-      //   testItem.Save();
-      //   string secondDescription = "Mow the lawn";
-      //
-      //   //Act
-      //   testItem.Edit(secondDescription);
-      //   string result = Item.Find(testItem.GetId()).GetDescription();
-      //
-      //   //Assert
-      //   Assert.AreEqual(secondDescription, result);
-      // }
-      //
-      // [TestMethod]
-      // public void GetCategoryId_ReturnsItemsParentCategoryId_Int()
-      // {
-      //   //Arrange
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Category newCategory = new Category("Home Tasks");
-      //   Item newItem = new Item("Walk the dog.", itemDueDate, newCategory.GetId());
-      //
-      //   //Act
-      //   int result = newItem.GetCategoryId();
-      //
-      //   //Assert
-      //   Assert.AreEqual(newCategory.GetId(), result);
-      // }
-      //
-      // [TestMethod]
-      // public void Delete_DeletesItemFromDatabase_List()
-      // {
-      //   //Arrange
-      //   DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //   Category newCategory = new Category("Home Tasks");
-      //   Item newItem1 = new Item("Walk the dog.", itemDueDate, 0, newCategory.GetId());
-      //   Item newItem2 = new Item("Clean the room", itemDueDate, 0, newCategory.GetId());
-      //   newItem1.Save();
-      //   newItem2.Save();
-      //   //Act
-      //   newItem1.Delete();
-      //   List<Item> result = Item.GetAll();
-      //   List<Item> newList = new List<Item> { newItem2 };
-      //   //Assert
-      //   CollectionAssert.AreEqual(newList, result);
-      // }
+      [TestMethod]
+      public void GetAll_ReturnsEmptyListFromDatabase_ClientList()
+      {
+        //Arrange
+        List<Client> newList = new List<Client> { };
+
+        //Act
+        List<Client> result = Client.GetAll();
+
+        //Assert
+        CollectionAssert.AreEqual(newList, result);
+      }
+
   }
 
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,204 +7,70 @@ namespace HairSalon.Models
 
     public class Client
     {
-      //   private string _name;
-      //   private int _id;
-      //   private int _stylistId;
-      //
-      //
-      // public Client (string name, int stylistId,  int id = 0)
-      // {
-      //     _name = name;
-      //     _stylistId = stylistId;
-      //     _id = id;
-      // }
+        private string _name;
+        private int _id;
+        // private int _stylistId;
 
-      // public string GetName()
-      // {
-      //     return _Name;
-      // }
-      //
-      // public void SetDescription(string newDescription)
-      // {
-      //     _description = newDescription;
-      // }
-      //
-      // public string GetDueDate()
-      // {
-      //     var dueDateToString = _dueDate.ToString("D");
-      //     return dueDateToString;
-      // }
-      //
-      // public void SetDueDate(DateTime dueDate)
-      // {
-      //     _dueDate = dueDate;
-      // }
-      //
-      // public int GetId()
-      // {
-      //     return _id;
-      // }
+
+      public Client (string name, int id = 0)
+      // int stylistId
+      {
+          _name = name;
+          // _stylistId = stylistId;
+          _id = id;
+      }
+
+      public string GetName()
+      {
+          return _name;
+      }
+
+      public void SetName(string newName)
+      {
+          _name = newName;
+      }
+
+      public int GetId()
+      {
+          return _id;
+      }
       //
       // public int GetCategoryId()
       // {
       //     return _categoryId;
       // }
 
-      // public static List<Item> GetAll()
-      // {
-      //     List<Item> allItems = new List<Item> {};
-      //     MySqlConnection conn = DB.Connection();
-      //     conn.Open();
-      //     var cmd = conn.CreateCommand() as MySqlCommand;
-      //     cmd.CommandText = @"SELECT * FROM items;";
-      //     var rdr = cmd.ExecuteReader() as MySqlDataReader;
-      //     while(rdr.Read())
-      //     {
-      //         int itemId = rdr.GetInt32(0);
-      //         string itemDescription = rdr.GetString(1);
-      //         DateTime itemDueDate = rdr.GetDateTime(2);
-      //         int itemCategoryId = rdr.GetInt32(3);
-      //         Item newItem = new Item(itemDescription, itemDueDate, itemCategoryId, itemId);
-      //         allItems.Add(newItem);
-      //     }
-      //     conn.Close();
-      //     if (conn != null)
-      //     {
-      //         conn.Dispose();
-      //     }
-      //     return allItems;
-      // }
-      //
-      // public static void ClearAll()
-      // {
-      //     MySqlConnection conn = DB.Connection();
-      //     conn.Open();
-      //     var cmd = conn.CreateCommand() as MySqlCommand;
-      //     cmd.CommandText = @"DELETE FROM items;";
-      //     cmd.ExecuteNonQuery();
-      //     conn.Close();
-      //     if (conn != null)
-      //     {
-      //         conn.Dispose();
-      //     }
-      // }
-      //
-      // public static Item Find(int id)
-      // {
-      //     MySqlConnection conn = DB.Connection();
-      //     conn.Open();
-      //     var cmd = conn.CreateCommand() as MySqlCommand;
-      //     cmd.CommandText = @"SELECT * FROM items WHERE id = (@searchId);";
-      //     MySqlParameter searchId = new MySqlParameter();
-      //     searchId.ParameterName = "@searchId";
-      //     searchId.Value = id;
-      //     cmd.Parameters.Add(searchId);
-      //     var rdr = cmd.ExecuteReader() as MySqlDataReader;
-      //     int itemId = 0;
-      //     string itemName = "";
-      //     int itemCategoryId = 0;
-      //     DateTime itemDueDate =  new DateTime(1999, 12, 24);
-      //     while(rdr.Read())
-      //     {
-      //         itemId = rdr.GetInt32(0);
-      //         itemName = rdr.GetString(1);
-      //         itemDueDate = rdr.GetDateTime(2);
-      //         itemCategoryId = rdr.GetInt32(3);
-      //     }
-      //     Item newItem = new Item(itemName, itemDueDate, itemCategoryId, itemId);
-      //     conn.Close();
-      //     if (conn != null)
-      //     {
-      //         conn.Dispose();
-      //     }
-      //     return newItem;
-      // }
-      //
-      // public override bool Equals(System.Object otherItem)
-      // {
-      //     if (!(otherItem is Item))
-      //     {
-      //         return false;
-      //     }
-      //     else
-      //     {
-      //         Item newItem = (Item) otherItem;
-      //         bool idEquality = this.GetId() == newItem.GetId();
-      //         bool descriptionEquality = this.GetDescription() == newItem.GetDescription();
-      //         bool dueDateEquality = (this.GetDueDate() == newItem.GetDueDate());
-      //         bool categoryEquality = this.GetCategoryId() == newItem.GetCategoryId();
-      //         return (idEquality && descriptionEquality && dueDateEquality && categoryEquality);
-      //      }
-      // }
-      //
-      // public void Save()
-      // {
-      //     MySqlConnection conn = DB.Connection();
-      //     conn.Open();
-      //     var cmd = conn.CreateCommand() as MySqlCommand;
-      //     cmd.CommandText = @"INSERT INTO items (description, dueDate, category_id) VALUES (@description, @dueDate, @category_id);";
-      //     MySqlParameter description = new MySqlParameter();
-      //     description.ParameterName = "@description";
-      //     description.Value = this._description;
-      //     cmd.Parameters.Add(description);
-      //     MySqlParameter categoryId = new MySqlParameter();
-      //     categoryId.ParameterName = "@category_id";
-      //     categoryId.Value = this._categoryId;
-      //     cmd.Parameters.Add(categoryId);
-      //     MySqlParameter dueDate = new MySqlParameter();
-      //     dueDate.ParameterName = "@dueDate";
-      //     dueDate.Value = this._dueDate;
-      //     cmd.Parameters.Add(dueDate);
-      //     cmd.ExecuteNonQuery();
-      //     _id = (int) cmd.LastInsertedId;
-      //     conn.Close();
-      //     if (conn != null)
-      //     {
-      //         conn.Dispose();
-      //     }
-      // }
-      //
-      // public void Edit(string newDescription)
-      // {
-      //     MySqlConnection conn = DB.Connection();
-      //     conn.Open();
-      //     var cmd = conn.CreateCommand() as MySqlCommand;
-      //     cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
-      //     MySqlParameter searchId = new MySqlParameter();
-      //     searchId.ParameterName = "@searchId";
-      //     searchId.Value = _id;
-      //     cmd.Parameters.Add(searchId);
-      //     MySqlParameter description = new MySqlParameter();
-      //     description.ParameterName = "@newDescription";
-      //     description.Value = newDescription;
-      //     cmd.Parameters.Add(description);
-      //     cmd.ExecuteNonQuery();
-      //     _description = newDescription;
-      //     conn.Close();
-      //     if (conn != null)
-      //     {
-      //         conn.Dispose();
-      //     }
-      // }
-      //
-      // public void Delete()
-      // {
-      //   MySqlConnection conn = DB.Connection();
-      //   conn.Open();
-      //   var cmd = conn.CreateCommand() as MySqlCommand;
-      //   cmd.CommandText = @"DELETE FROM items WHERE id = @searchId;";
-      //   MySqlParameter searchId = new MySqlParameter();
-      //   searchId.ParameterName = "@searchId";
-      //   searchId.Value = _id;
-      //   cmd.Parameters.Add(searchId);
-      //   cmd.ExecuteNonQuery();
-      //   conn.Close();
-      //   if (conn != null)
-      //   {
-      //       conn.Dispose();
-      //   }
-      // }
+      public static List<Client> GetAll()
+      // this is where items from the database will go
+      {
+          List<Client> allClients = new List<Client> {};
+          MySqlConnection conn = DB.Connection();
+          //calling that database connection
+          conn.Open();
+          //opens database
+          MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+          //new instance of special object to call commands on db objects
+          cmd.CommandText = @"SELECT * FROM clients;";
+          //sql commands
+          MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+          //creates a data reader object - responsible for reading data returned by our database
+          while(rdr.Read())
+          {
+            string clientName = rdr.GetString(1);
+            int clientId = rdr.GetInt32(0);
+            //once we collect the data we can use it to instanciate a new client object and add them to our allClients List
+            Client newClient = new Client(clientName, clientId);
+            allClients.Add(newClient);
+          }
+          //to close the database
+          conn.Close();
+
+          if (conn != null)
+          {
+            conn.Dispose();
+          }
+          return allClients;
+        }
 
     }
-
 }
